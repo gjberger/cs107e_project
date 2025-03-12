72c84932bd81db8d9219dc78594a093d9129625b
Gabriel Berger <gabrieljberger@gmail.com>
2025-03-12 03:39:19 -0700

    added character

--- game.c
+++ game.c
@@ -6,8 +6,13 @@
 const int WIDTH = 400;
 const int HEIGHT = 600;
 
+
+
 void draw_background(void) {
     gl_clear(GL_WHITE);
+
+    gl_draw_line(WIDTH / 8, 0.25 * HEIGHT, (7 * WIDTH) / 8, 0.25 * HEIGHT, GL_BLACK);
+    
 
     gl_draw_line(0, HEIGHT, WIDTH / 8, 0.75 * HEIGHT, GL_BLACK);
     gl_draw_line(WIDTH / 8, 0.75 * HEIGHT, WIDTH / 8, 0.25 * HEIGHT, GL_BLACK);
@@ -18,10 +23,19 @@ void draw_background(void) {
     gl_draw_string(WIDTH / 4, 0.15 * HEIGHT, "CS107E Surfers", GL_BLACK);
 
    
-    gl_draw_circle(WIDTH / 2, 0.75 * HEIGHT, 10, GL_BLACK);
 
     gl_swap_buffer();
 
+}
+
+void draw_character(void) {
+    gl_swap_buffer();
+    gl_draw_circle(WIDTH / 2, 0.87 * HEIGHT, 15, GL_BLACK);
+    gl_draw_line(WIDTH / 2, 0.89 * HEIGHT, WIDTH / 2, 0.95 * HEIGHT, GL_BLACK);
+    gl_draw_line(WIDTH / 2, 0.95 * HEIGHT, (WIDTH / 2) - 7, 0.98 * HEIGHT, GL_BLACK);
+    gl_draw_line(WIDTH / 2, 0.95 * HEIGHT, (WIDTH / 2) + 7, 0.98 * HEIGHT, GL_BLACK);
+    
+    gl_swap_buffer();
 }
 
 void main(void) {
@@ -31,6 +45,8 @@ void main(void) {
 
     gl_init(WIDTH, HEIGHT, GL_DOUBLEBUFFER);
     draw_background();
+    draw_character();
+    
     while (1) {
 
     }
